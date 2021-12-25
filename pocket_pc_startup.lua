6265c5eb82c536dafbd48f0cbef6aa5b5410944e
aujuslia <julialcraft@gmail.com>
2021-12-24 17:21:55 -0800

    f

--- pocket_pc_startup.lua
+++ pocket_pc_startup.lua
@@ -1,5 +1,9 @@
-term.setCursorPos(1,1)
-term.clear()
+function clear()
+    term.setCursorPos(1,1)
+    term.clear()
+end
+
+clear()
 
 term.setTextColor(colors.purple)
 print("JuliOS 0.1")
@@ -11,6 +15,8 @@ while true do
 
     event, key, isHeld = os.pullEvent("key")
     if key == keys.s then
+        clear()
+
         term.setTextColor(colors.orange)
         print("Shell:")
         error()

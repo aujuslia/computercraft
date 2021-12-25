16815057061e90dc6de356d3939dbc099b6c0772
aujuslia <julialcraft@gmail.com>
2021-12-24 17:13:08 -0800

    sdf

--- pocket_pc_startup.lua
+++ pocket_pc_startup.lua
@@ -2,5 +2,16 @@ term.setCursorPos(1,1)
 term.clear()
 
 term.setTextColor(colors.purple)
-
 print("JuliOS 0.1")
+
+
+--main loop:
+
+while true do
+
+    event, key, isHeld = os.pullEvent("key")
+    if key == keys.s then
+        print("Exiting to shell:")
+        error()
+    end
+end

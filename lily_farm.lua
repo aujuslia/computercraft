7885a8470e5b6ad12896a459f62a1118005ee17c
aujuslia <julialcraft@gmail.com>
2022-01-11 22:47:51 -0800

    4

--- lily_farm.lua
+++ lily_farm.lua
@@ -1,16 +1,17 @@
 function harvest_row()
-    for rowIndex=1, 4 do
+    for rowIndex=0, 3 do
+        itemSel = (rowIndex * 2) + 1
         for cropIndex=1, 4 do
             for i=1, 2 do --one for down, one for forward
-                turtle.select(rowIndex)
+                turtle.select(itemSel)
                 if i == 1 then
                     turtle.digDown()
                 else
                     turtle.dig()
                 end
 
-                if turtle.getItemCount(1 + rowIndex) > 0 then
-                    turtle.select(1 + rowIndex)
+                if turtle.getItemCount(1 + itemSel) > 0 then
+                    turtle.select(1 + itemSel)
                 end
 
                 if i == 1 then
@@ -19,7 +20,7 @@ function harvest_row()
                     turtle.place()
                 end
 
-                turtle.select(rowIndex)
+                turtle.select(itemSel)
 
                 if i == 1 then
                     turtle.turnLeft()
@@ -39,3 +40,4 @@ function harvest()
     turtle.forward()
     harvest_row()
 end
+harvest()

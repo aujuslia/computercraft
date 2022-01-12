6421c927559bea0acec20c57590a6931a22f84ac
aujuslia <julialcraft@gmail.com>
2022-01-11 22:56:01 -0800

    sdf

--- lily_farm.lua
+++ lily_farm.lua
@@ -1,36 +1,38 @@
 function harvest_row()
-    for rowIndex=0, 3 do
-        itemSel = (rowIndex * 2) + 1
-        for cropIndex=1, 4 do
-            for i=1, 2 do --one for down, one for forward
-                turtle.select(itemSel)
-                if i == 1 then
-                    turtle.digDown()
-                else
-                    turtle.dig()
-                end
+    for heightIndex = 1, 2 do
+        for rowIndex=0, 3 do
+            itemSel = (rowIndex * 2 * heightIndex) + 1
+            for cropIndex=1, 4 do
+                for i=1, 2 do --one for down, one for forward
+                    turtle.select(itemSel)
+                    if i == 1 then
+                        turtle.digDown()
+                    else
+                        turtle.dig()
+                    end
 
-                if turtle.getItemCount(1 + itemSel) > 0 then
-                    turtle.select(1 + itemSel)
-                end
+                    if turtle.getItemCount(1 + itemSel) > 0 then
+                        turtle.select(1 + itemSel)
+                    end
 
-                if i == 1 then
-                    turtle.placeDown()
-                else
-                    turtle.place()
-                end
+                    if i == 1 then
+                        turtle.placeDown()
+                    else
+                        turtle.place()
+                    end
 
-                turtle.select(itemSel)
+                    turtle.select(itemSel)
 
-                if i == 1 then
-                    turtle.turnLeft()
-                else
-                    turtle.turnRight()
+                    if (i == 1 and heightIndex == 1) or (i == 2 and heightIndex == 2) then
+                        turtle.turnLeft()
+                    else
+                        turtle.turnRight()
+                    end
                 end
+                turtle.forward()
             end
             turtle.forward()
         end
-        turtle.forward()
     end
 end
 
